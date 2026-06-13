a6bfb359b3ab04a21427d06d9871976fb30f110b
VinogradRustem <guapyala@gmail.com>
2026-06-13 13:49:10 +0700

    disable locale detection

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,8 @@ import createMiddleware from 'next-intl/middleware';
 export default createMiddleware({
   locales: ['en', 'ru'],
   defaultLocale: 'en',
-  localePrefix: 'as-needed'
+  localePrefix: 'as-needed',
+  localeDetection: false
 });
 
 export const config = {
